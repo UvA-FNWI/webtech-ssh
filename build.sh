55763522ce110da94ecf206a03a8e3fbd30d1d5b
Sam van Kampen <sam@tehsvk.net>
2023-12-18 15:12:23 +0100

    build.sh: separately build targets
    I swear space-separated targets worked when I tried them, but perhaps
    it was overridden by .cargo/config or something. We can still build the
    targets separately, though, so whatever.

--- build.sh
+++ build.sh
@@ -5,9 +5,11 @@ platform=$(uname)
 
 case "$platform" in
     "Linux")
-        CARGO_BUILD_TARGET='aarch64-unknown-linux-musl x86_64-unknown-linux-musl' cargo build "$@"
+        CARGO_BUILD_TARGET=aarch64-unknown-linux-musl cargo build "$@"
+        CARGO_BUILD_TARGET=x86_64-unknown-linux-musl cargo build "$@"
         ;;
     "Darwin")
-        CARGO_BUILD_TARGET='aarch64-apple-darwin x86_64-apple-darwin' cargo build "$@"
+        CARGO_BUILD_TARGET=aarch64-apple-darwin cargo build "$@"
+        CARGO_BUILD_TARGET=x86_64-apple-darwin cargo build "$@"
         ;;
 esac
